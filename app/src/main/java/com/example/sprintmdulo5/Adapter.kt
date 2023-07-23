f17ac46c2af1d6a651824c481078a83404f99331
Margaret <maenunezt@gmail.com>
2023-07-22 22:17:23 -0400

    incorporación de las dependencias de coil para poder mostrar las imágenes referentes a los productos. Creación de la clase Adapter, la cual hereda de RecyclerView.Adapter. Creación de los métodos: onCreateViewHolder, getItemCount y onBindViewHolder. Creación de la clase anidada ViewHolder que hereda de la clase ViewHolder.

---
+++ app/src/main/java/com/example/sprintmdulo5/Adapter.kt
@@ -0,0 +1,24 @@
+package com.example.sprintmdulo5
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class Adapter: RecyclerView.Adapter<Adapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Adapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: Adapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class ViewHolder {
+
+    }
+
+}
